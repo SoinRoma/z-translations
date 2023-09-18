e3d8960b2446a0ac49a6af2ceb80960ca879c464
SoinRoma <soinroma26@gmail.com>
2023-09-18 16:19:03 +0500

    - added modal and script for second form

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -3,230 +3,247 @@ function burgerMenu() {
 }
 
 function scrollSmooth() {
-  const anchors = document.querySelectorAll('a[href*="#"]')
+    const anchors = document.querySelectorAll('a[href*="#"]')
 
-  for (let anchor of anchors) {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault()
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault()
 
-      const blockID = anchor.getAttribute('href').substr(1)
+            const blockID = anchor.getAttribute('href').substr(1)
 
-      document.getElementById(blockID).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      })
-    })
-  }
+            document.getElementById(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    }
 }
 
 function initSlider() {
-  document.addEventListener('DOMContentLoaded', function () {
-    const splide = new Splide('.splide', {
-      type: 'loop',
-      rewind: true,
-      pagination: false,
-      perPage: 3,
-      gap: '60px',
-      arrows: false,
-    });
-    const bar = splide.root.querySelector('.my-slider-progress-bar')
+    document.addEventListener('DOMContentLoaded', function () {
+        const splide = new Splide('.splide', {
+            type: 'loop',
+            rewind: true,
+            pagination: false,
+            perPage: 3,
+            gap: '60px',
+            arrows: false,
+        });
+        const bar = splide.root.querySelector('.my-slider-progress-bar')
 
-    //attach events to custom buttons
-    document.querySelector('.splide_next').addEventListener('click', e => {
-      splide.go('+1')
+        //attach events to custom buttons
+        document.querySelector('.splide_next').addEventListener('click', e => {
+            splide.go('+1')
+        })
+
+        document.querySelector('.splide_prev').addEventListener('click', e => {
+            splide.go('-1')
+        })
+
+
+        splide.on('mounted move', function () {
+            const end = splide.Components.Controller.getEnd() + 3
+            const rate = Math.min((splide.index + 1) / end, 1)
+            bar.style.width = String(100 * rate) + '%'
+        })
+
+        splide.mount()
     })
-
-    document.querySelector('.splide_prev').addEventListener('click', e => {
-      splide.go('-1')
-    })
-
-
-
-    splide.on('mounted move', function () {
-      const end = splide.Components.Controller.getEnd() + 3
-      const rate = Math.min((splide.index + 1) / end, 1)
-      bar.style.width = String(100 * rate) + '%'
-    })
-
-    splide.mount()
-  })
 }
 
 function initSelects() {
-  const langFromArray = []
-  const langToArray = []
-  const typeArray = []
+    const langFromArray = []
+    const langToArray = []
+    const typeArray = []
 
-  $('#language_from option').each(function(){
-    const img = $(this).attr("data-thumbnail")
-    const text = this.innerText
-    const item = `<li class="d-flex justify-content-between w-100">
+    $('#language_from option').each(function () {
+        const img = $(this).attr("data-thumbnail")
+        const text = this.innerText
+        const item = `<li class="d-flex justify-content-between w-100">
                     <div><img src="${img}" alt="icon"/><span>${text}</span></div>
                     <img src="./static/assets/images/svg/select-arrow.svg" alt="icon" class="select-arrow" style="width: 10px"/>
                   </li>`
-    langFromArray.push(item)
-  })
+        langFromArray.push(item)
+    })
 
-  $('#language_to option').each(function(){
-    const img = $(this).attr("data-thumbnail")
-    const text = this.innerText
-    const item = `<li class="d-flex justify-content-between w-100">
+    $('#language_to option').each(function () {
+        const img = $(this).attr("data-thumbnail")
+        const text = this.innerText
+        const item = `<li class="d-flex justify-content-between w-100">
                     <div><img src="${img}" alt="icon"/><span>${text}</span></div>
                     <img src="./static/assets/images/svg/select-arrow.svg" class="select-arrow" alt="icon" style="width: 10px"/>
                   </li>`
-    langToArray.push(item)
-  })
+        langToArray.push(item)
+    })
 
-  $('#type option').each(function(){
-    const text = this.innerText
-    const item = `<li class="d-flex justify-content-between w-100">
+    $('#type option').each(function () {
+        const text = this.innerText
+        const item = `<li class="d-flex justify-content-between w-100">
                     <span>${text}</span>
                     <img src="./static/assets/images/svg/select-arrow.svg" class="select-arrow" alt="icon" style="width: 10px"/>
                   </li>`
-    typeArray.push(item)
-  })
+        typeArray.push(item)
+    })
 
-  $('#languages_from_list').html(langFromArray)
-  $('#languages_to_list').html(langToArray)
-  $('#type_list').html(typeArray)
+    $('#languages_from_list').html(langFromArray)
+    $('#languages_to_list').html(langToArray)
+    $('#type_list').html(typeArray)
 
-  //Set the button value to the first el of the array
-  $('#change_select1').html(langFromArray[0]).attr('value', '1')
-  $('#change_select2').html(langToArray[0]).attr('value', '1')
-  $('#change_select3').html(typeArray[0]).attr('value', '1')
+    //Set the button value to the first el of the array
+    $('#change_select1').html(langFromArray[0]).attr('value', '1')
+    $('#change_select2').html(langToArray[0]).attr('value', '1')
+    $('#change_select3').html(typeArray[0]).attr('value', '1')
 
 //change button stuff on click
-  $('#languages_from_list li').click(function(){
-    const img = $(this).find('img').attr("src")
-    const value = $(this).find('img').attr('value')
-    const text = this.innerText
-    const item = `<li class="d-flex justify-content-between w-100">
+    $('#languages_from_list li').click(function () {
+        const img = $(this).find('img').attr("src")
+        const value = $(this).find('img').attr('value')
+        const text = this.innerText
+        const item = `<li class="d-flex justify-content-between w-100">
                     <div><img src="${img}" alt="icon"/><span>${text}</span></div>
                     <img src="./static/assets/images/svg/select-arrow.svg" class="select-arrow" alt="icon" style="width: 10px"/>
                   </li>`
-    $('#change_select1').html(item).attr('value', value)
-    $("#list_1").toggle()
-  })
+        $('#change_select1').html(item).attr('value', value)
+        $("#list_1").toggle()
+    })
 
-  $('#languages_to_list li').click(function(){
-    const img = $(this).find('img').attr("src")
-    const value = $(this).find('img').attr('value')
-    const text = this.innerText
-    const item = `<li class="d-flex justify-content-between w-100">
+    $('#languages_to_list li').click(function () {
+        const img = $(this).find('img').attr("src")
+        const value = $(this).find('img').attr('value')
+        const text = this.innerText
+        const item = `<li class="d-flex justify-content-between w-100">
                     <div><img src="${img}" alt="icon"/><span>${text}</span></div>
                     <img src="./static/assets/images/svg/select-arrow.svg" class="select-arrow" alt="icon" style="width: 10px"/>
                   </li>`
-    $('#change_select2').html(item).attr('value', value)
-    $("#list_2").toggle()
-  })
+        $('#change_select2').html(item).attr('value', value)
+        $("#list_2").toggle()
+    })
 
-  $('#type_list li').click(function(){
-    const value = $(this).find('img').attr('value')
-    const text = this.innerText
-    const item = `<li class="d-flex justify-content-between w-100">
+    $('#type_list li').click(function () {
+        const value = $(this).find('img').attr('value')
+        const text = this.innerText
+        const item = `<li class="d-flex justify-content-between w-100">
                     <span>${text}</span>
                     <img src="./static/assets/images/svg/select-arrow.svg" class="select-arrow" alt="icon" style="width: 10px"/>
                   </li>`
-    $('#change_select3').html(item).attr('value', value)
-    $("#list_3").toggle()
-  })
+        $('#change_select3').html(item).attr('value', value)
+        $("#list_3").toggle()
+    })
 
-  $("#change_select1").click(function(){
-    $("#list_1").toggle()
-  })
+    $("#change_select1").click(function () {
+        $("#list_1").toggle()
+    })
 
-  $("#change_select2").click(function(){
-    $("#list_2").toggle()
-  })
+    $("#change_select2").click(function () {
+        $("#list_2").toggle()
+    })
 
-  $("#change_select3").click(function(){
-    $("#list_3").toggle()
-  })
+    $("#change_select3").click(function () {
+        $("#list_3").toggle()
+    })
 
-  window.addEventListener('click', function(e){
-    if (!document.getElementById('change_select1').contains(e.target)){
-      $("#list_1").hide()
-    }
-    if (!document.getElementById('change_select2').contains(e.target)){
-      $("#list_2").hide()
-    }
-    if (!document.getElementById('change_select3').contains(e.target)){
-      $("#list_3").hide()
-    }
-  })
+    window.addEventListener('click', function (e) {
+        if (!document.getElementById('change_select1').contains(e.target)) {
+            $("#list_1").hide()
+        }
+        if (!document.getElementById('change_select2').contains(e.target)) {
+            $("#list_2").hide()
+        }
+        if (!document.getElementById('change_select3').contains(e.target)) {
+            $("#list_3").hide()
+        }
+    })
 
 }
 
 function initFiles() {
-  const uppy = new Uppy.Uppy({
-    locale: Uppy.locales.ru_RU,
-    restrictions: {
-      maxNumberOfFiles: 1,
-    }
-  })
-  uppy.use(Uppy.Dashboard,{
-    target: '#uppy',
-    inline: true,
-    allowMultipleUploadBatches: false,
-    width: '524px',
-    height: '200px',
-    hideUploadButton: true,
-    hideProgressAfterFinish: true,
-    hideCancelButton: true,
-    disableInformer: true,
-    disableStatusBar: true,
-    disableThumbnailGenerator: true,
-    locale: {
-      strings: {
-        dropPasteFiles: `
+    const uppy = new Uppy.Uppy({
+        locale: Uppy.locales.ru_RU,
+        restrictions: {
+            maxNumberOfFiles: 1,
+        }
+    })
+    uppy.use(Uppy.Dashboard, {
+        target: '#uppy',
+        inline: true,
+        allowMultipleUploadBatches: false,
+        width: '524px',
+        height: '200px',
+        hideUploadButton: true,
+        hideProgressAfterFinish: true,
+        hideCancelButton: true,
+        disableInformer: true,
+        disableStatusBar: true,
+        disableThumbnailGenerator: true,
+        locale: {
+            strings: {
+                dropPasteFiles: `
         Перетащите документ сюда или %{browseFiles} его
         `
-      }
-    }
-  })
+            }
+        }
+    })
 
-  const uppy2 = new Uppy.Uppy({
-    locale: Uppy.locales.ru_RU,
-    restrictions: {
-      maxNumberOfFiles: 1,
-    }
-  })
-  uppy2.use(Uppy.Dashboard,{
-    target: '#uppy2',
-    inline: true,
-    allowMultipleUploadBatches: false,
-    width: '524px',
-    height: '200px',
-    hideUploadButton: true,
-    hideRetryButton: true,
-    hidePauseResumeButton: true,
-    hideProgressAfterFinish: true,
-    hideCancelButton: true,
-    disableInformer: true,
-    disableStatusBar: true,
-    disableThumbnailGenerator: true,
-    locale: {
-      strings: {
-        dropPasteFiles: `
-        Перетащите документ сюда или %{browseFiles} его
-        `
-      }
-    }
-  })
+
 }
 
 function firstForm() {
-  $('.price-form').submit((e)=>{
-    e.preventDefault()
-  })
+    $('.price-form').submit((e) => {
+        e.preventDefault()
+    })
 }
 
 
 function secondForm() {
-  $('.cooperation-form').submit((e)=>{
-    e.preventDefault()
-  })
+    const uppy2 = new Uppy.Uppy({
+        locale: Uppy.locales.ru_RU,
+        restrictions: {
+            maxNumberOfFiles: 1,
+        }
+    })
+    uppy2.use(Uppy.Dashboard, {
+        target: '#uppy2',
+        inline: true,
+        allowMultipleUploadBatches: false,
+        width: '524px',
+        height: '200px',
+        hideUploadButton: true,
+        hideRetryButton: true,
+        hidePauseResumeButton: true,
+        hideProgressAfterFinish: true,
+        hideCancelButton: true,
+        disableInformer: true,
+        disableStatusBar: true,
+        disableThumbnailGenerator: true,
+        locale: {
+            strings: {
+                dropPasteFiles: `
+        Перетащите документ сюда или %{browseFiles} его
+        `
+            }
+        }
+    })
+
+    $('.cooperation-form').submit((e) => {
+        e.preventDefault()
+        const file = uppy2.getFiles()
+        if (file.length) {
+            const fileID = file[0].id
+            $.magnificPopup.open({
+                items: {
+                    src: '#modal'
+                },
+                type: 'inline'
+            })
+
+            uppy2.removeFile(fileID)
+        }
+    })
 }
+
+$(".closeBtn").click(function () {
+    $.magnificPopup.close()
+})
 
 
 burgerMenu()
